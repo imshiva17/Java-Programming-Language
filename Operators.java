a3878c61f102a2770cb541e732c1c5a83004ead7
imshiva17 <shivarajak277@gmail.com>
2026-04-02 07:29:56 +0530

    Operators

---
+++ Operators.java
@@ -0,0 +1,123 @@
+// Arithmetic Operators
+
+// Binary Operators
+
+// public class Operators {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 5;
+
+//         // Addition
+
+//         System.out.println("Sum : " + (a + b));
+
+//         // Subtraction
+
+//         System.out.println("Subtraction : " + (a - b));
+
+//         // Multiplication
+
+//         System.out.println("Multiplication : " + (a * b));
+
+//         // Division
+
+//         System.out.println("Division : " + (a / b));
+
+//         // Modulus
+
+//         System.out.println("Modulus : " + (a % b));
+//     }
+// }
+
+// Uanry Operators
+
+// public class Operators {
+//     public static void main(String[] args) {
+//         // Pre-increment
+//         // int a = 10;
+//         // int b = ++a;
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // Post-increment
+//         // int a = 10;
+//         // int b = a++;
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // Pre-decrement
+//         // int a = 10;
+//         // int b = --a;
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // Post-decrement
+//         int a = 10;
+//         int b = a--;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
+
+//Relational Operators
+
+// public class Operators {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         int b = 5;
+
+//         // Equal to(==)
+//         System.out.println(a == b);
+
+//         // Not equal to (!=)
+//         System.out.println(a != b);
+
+//         // Greater than
+//         System.out.println(a > b);
+
+//         // Lesser than
+//         System.out.println(a < b);
+
+//         // Greater than and equal to
+//         System.out.println(a >= b);
+
+//         // Lesser than and equal to
+//         System.out.println(a <= b);
+
+//     }
+// }
+
+//Logical Operators
+
+// public class Operators {
+//     public static void main(String args[]) {
+//         // Logical AND (&&)
+//         // System.out.println((3 > 2) && (5 > 3));
+//         // System.out.println((6 > 9) && (3 < 6));
+
+//         // Logical OR (||)
+//         // System.out.println((3 > 2) || (5 > 3));
+//         // System.out.println((6 > 9) || (3 < 6));
+
+//         // Logical NOT (!)
+//         // System.out.println(!(3 > 2));
+//         // System.out.println((3 > 2));
+
+//     }
+// }
+
+//Assignment Operators
+
+// public class Operators {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         // System.out.println(a);
+//         // System.out.println(a += 10);
+//         // System.out.println(a -= 10);
+//         // System.out.println(a *= 10);
+//         // System.out.println(a /= 10);
+//         System.out.println(a %= 10);
+
+//     }
+// }
+
