86985585dd83b8f6dc45b71998e12232f147de6a
imshiva17 <shivarajak277@gmail.com>
2026-04-03 22:11:18 +0530

    linear search

--- LinearSearch.java
+++ LinearSearch.java
@@ -257,7 +257,7 @@
 //     }
 // }
 
-//You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has. A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+//leetcode 1672. You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has. A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
 
 import java.util.*;
 
