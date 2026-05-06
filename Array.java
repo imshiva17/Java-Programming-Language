2aef930fcb250af1e4473027719a5b539aed0e94
imshiva17 <shivarajak277@gmail.com>
2026-05-07 00:05:36 +0530

    FIX ERROR

--- Array.java
+++ Array.java
@@ -288,35 +288,35 @@
 
 // Max Subarray Sum (Brute Force)
 
-// import java.util.*;
+import java.util.*;
 
-// public class Array {
-//     public static void maxSubarraySum(int num[]) {
-//         int currSum = 0;
-//         int maxSum = Integer.MIN_VALUE;
-//         for (int i = 0; i < num.length; i++) {
-//             int start = i;
-//             for (int j = i; j < num.length; j++) {
-//                 int last = j;
-//                 currSum = 0;
-//                 for (int k = start; k <= last; k++) {
-//                     currSum += num[k];
-//                 }
-//                 System.out.println(currSum);
-//                 if (maxSum < currSum) {
-//                     maxSum = currSum;
-//                 }
-//             }
-//             System.out.println();
-//         }
-//         System.out.println("Max sum: " + maxSum);
-//     }
+public class Array {
+    public static void maxSubarraySum(int num[]) {
+        int currSum;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < num.length; i++) {
+            int start = i;
+            for (int j = i; j < num.length; j++) {
+                int last = j;
+                currSum = 0;
+                for (int k = start; k <= last; k++) {
+                    currSum += num[k];
+                }
+                System.out.println(currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Max sum: " + maxSum);
+    }
 
-//     public static void main(String[] args) {
-//         int num[] = { 1, -2, 6, -1, 3 };
-//         maxSubarraySum(num);
-//     }
-// }
+    public static void main(String[] args) {
+        int num[] = { 1, -2, 6, -1, 3 };
+        maxSubarraySum(num);
+    }
+}
 
 // Max Subarray Sum(Prefix Sum)
 
