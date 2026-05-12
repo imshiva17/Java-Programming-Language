12e092ff180e130ba7f62caa7d75fe5a47aa0a19
imshiva17 <shivarajak277@gmail.com>
2026-05-12 08:03:17 +0530

    Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -36,51 +36,88 @@
 
 //Order Agnostic Bianry Search
 
-import java.util.*;
+// import java.util.*;
 
-public class BinarySearch {
+// public class BinarySearch {
 
-    public static int agnosticSearchInArray(int arr[], int target) {
+//     public static int agnosticSearchInArray(int arr[], int target) {
 
-        int start = 0, end = arr.length - 1;
+//         int start = 0, end = arr.length - 1;
 
-        // find whether the array is sorted in ascending or descending
-        boolean isAsc = arr[start] < arr[end];
+//         // find whether the array is sorted in ascending or descending
+//         boolean isAsc = arr[start] < arr[end];
 
-        while (start <= end) {
-            
-            int mid = start + (end - start) / 2;
+//         while (start <= end) {
 
-            if (arr[mid] == target) {
-                return mid;
-            }
+//             int mid = start + (end - start) / 2;
 
-            if (isAsc) {
-                if (target > arr[mid]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            } else {
-                if (target < arr[mid]) {
+//             if (arr[mid] == target) {
+//                 return mid;
+//             }
 
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            }
+//             if (isAsc) {
+//                 if (target > arr[mid]) {
+//                     start = mid + 1;
+//                 } else {
+//                     end = mid - 1;
+//                 }
+//             } else {
+//                 if (target < arr[mid]) {
 
-        }
+//                     start = mid + 1;
+//                 } else {
+//                     end = mid - 1;
+//                 }
+//             }
 
-        return -1;
+//         }
 
-    }
+//         return -1;
 
-    public static void main(String args[]) {
-        int arr[] = { 90, 75, 18, 12, 6, 4, 3, 1 };
-        int target = 75;
+//     }
 
-        System.out.print(agnosticSearchInArray(arr, target));
+//     public static void main(String args[]) {
+//         int arr[] = { 90, 75, 18, 12, 6, 4, 3, 1 };
+//         int target = 75;
 
-    }
-}
+//         System.out.print(agnosticSearchInArray(arr, target));
+
+//     }
+// }
+
+//Binary search in sorted 2d matrix 
+
+// import java.util.*;
+
+// public class BinarySearch {
+
+//     public static int[] search(int matrix[][], int target) {
+//         int row = 0;
+//         int col = matrix[0].length - 1;
+
+//         while (row < matrix.length && col >= 0) {
+//             if (matrix[row][col] == target) {
+//                 return new int[] { row, col };
+//             } else if (matrix[row][col] > target) {
+//                 col--;
+//             } else {
+//                 row++;
+//             }
+//         }
+
+//         return new int[] { -1, -1 };
+//     }
+
+//     public static void main(String[] args) {
+//         int matrix[][] = {
+//                 { 10, 20, 30, 40 },
+//                 { 15, 25, 35, 45 },
+//                 { 27, 29, 37, 48 },
+//                 { 32, 33, 39, 50 },
+//         };
+
+//         int target = 39;
+//         System.out.print(Arrays.toString(search(matrix, target)));
+
+//     }
+// }
