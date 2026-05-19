e4f19b34da20d65b3af95d8e4d85099076cbb807
imshiva17 <shivarajak277@gmail.com>
2026-05-19 08:08:11 +0530

    Set Mismatch

--- Sorting.java
+++ Sorting.java
@@ -416,37 +416,78 @@
 // Input: nums = [1,1,2]
 // Output: [1]
 
-import java.util.*;
+// import java.util.*;
 
-public class Sorting {
-    public static ArrayList<Integer> findAllDuplicates(int nums[]) {
+// public class Sorting {
+//     public static ArrayList<Integer> findAllDuplicates(int nums[]) {
 
-        int i = 0;
-        while (i < nums.length) {
-            int correct = nums[i] - 1;
-            if (nums[i] != nums[correct]) {
-                int temp = nums[i];
-                nums[i] = nums[correct];
-                nums[correct] = temp;
+//         int i = 0;
+//         while (i < nums.length) {
+//             int correct = nums[i] - 1;
+//             if (nums[i] != nums[correct]) {
+//                 int temp = nums[i];
+//                 nums[i] = nums[correct];
+//                 nums[correct] = temp;
 
-            } else {
-                i++;
-            }
-        }
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != j + 1) {
-                ans.add(nums[j]);
-            }
-        }
+//             } else {
+//                 i++;
+//             }
+//         }
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         for (int j = 0; j < nums.length; j++) {
+//             if (nums[j] != j + 1) {
+//                 ans.add(nums[j]);
+//             }
+//         }
 
-        return ans;
-    }
+//         return ans;
+//     }
 
-    public static void main(String[] args) {
-        int nums[] = { 4, 3, 2, 7, 8, 2, 3, 1 };
+//     public static void main(String[] args) {
+//         int nums[] = { 4, 3, 2, 7, 8, 2, 3, 1 };
 
-        System.out.println(findAllDuplicates(nums));
+//         System.out.println(findAllDuplicates(nums));
 
-    }
-}
+//     }
+// }
+
+// 645. Set Mismatch: You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number. You are given an integer array nums representing the data status of this set after the error. Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+// Example 1:
+// Input: nums = [1,2,2,4]
+// Output: [2,3]
+
+// Example 2:
+// Input: nums = [1,1]
+// Output: [1,2]
+
+// import java.util.*;
+
+// public class Sorting {
+//     public static int[] findErrorNums(int[] nums) {
+
+//         int i = 0;
+//         while (i < nums.length) {
+//             int correct = nums[i] - 1;
+//             if (nums[i] != nums[correct]) {
+//                 int temp = nums[i];
+//                 nums[i] = nums[correct];
+//                 nums[correct] = temp;
+//             } else {
+//                 i++;
+//             }
+//         }
+
+//         for (int j = 0; j < nums.length; j++) {
+//             if (nums[j] != j + 1) {
+//                 return new int[] { nums[j], j + 1 };
+//             }
+//         }
+//         return new int[] { -1, -1 };
+//     }
+
+//     public static void main(String[] args) {
+//         int nums[] = { 1, 2, 2, 4 };
+//         System.out.println(Arrays.toString(findErrorNums(nums)));
+//     }
+// }
