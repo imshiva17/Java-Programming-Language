f81db3bf3997d3e04cbf469f8fc05cf9fd95b6f0
imshiva17 <shivarajak277@gmail.com>
2026-05-20 07:33:14 +0530

    Started Strings Concept

---
+++ Strings.java
@@ -0,0 +1,13 @@
+// Strings
+
+public class Strings {
+    public static void main(String[] args) {
+        // Syntax
+
+        String name; // Declaration
+
+        name = "Shiva"; // Initialization
+
+        System.out.println(name);
+    }
+}
