306c9e747d3b4648cf9c4851c99cc40a39b31180
imshiva17 <shivarajak277@gmail.com>
2026-05-20 07:44:44 +0530

    Learn next and nextLine

--- Strings.java
+++ Strings.java
@@ -1,13 +1,32 @@
 // Strings
 
-public class Strings {
-    public static void main(String[] args) {
-        // Syntax
+// public class Strings {
+//     public static void main(String[] args) {
+//         // Syntax
 
-        String name; // Declaration
+//         String name; // Declaration
 
-        name = "Shiva"; // Initialization
+//         name = "Shiva"; // Initialization
 
-        System.out.println(name);
-    }
-}
+//         System.out.println(name);
+//     }
+// }
+
+// import java.util.*;
+
+// public class Strings {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // String name = sc.next();
+//         // System.out.println(name);
+//         // // I/P: shiva rajak
+//         // // O/P: shiva
+
+//         String name = sc.nextLine();
+//         System.out.println(name);
+//         // I/P: shiva rajak
+//         // O/P: shiva rajak
+//     }
+// }
