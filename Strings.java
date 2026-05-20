95c30530c1d45c8b350a83f4952be353d197d68c
imshiva17 <shivarajak277@gmail.com>
2026-05-20 07:51:23 +0530

    Learn concatenation

--- Strings.java
+++ Strings.java
@@ -33,18 +33,29 @@
 //     }
 // }
 
-
 //To find String length - length()
 
-import java.util.*;
+// import java.util.*;
+
+// public class Strings {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         String name = "Shiva Rajak";
+//         int length = name.length();    //it count space also
+//         System.out.println(length);
+//     }
+// }
+
+//Concatenation: means joining two things
 
 public class Strings {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String name = "Shiva Rajak";
-        int length = name.length();    //it count space also
-        System.out.println(length);
+        String firstName = "Shiva";
+        String lastName = "Rajak";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
     }
 }
