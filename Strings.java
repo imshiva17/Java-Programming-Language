3dff8d63b5a5d9c2fd6bb444f33758b5d68873ad
imshiva17 <shivarajak277@gmail.com>
2026-05-21 07:00:00 +0530

    Shortest Route

--- Strings.java
+++ Strings.java
@@ -81,21 +81,62 @@
 
 //Check string is palindrome or not
 
-public class Strings {
+// public class Strings {
 
-    public static boolean isPalindrome(String str) {
-        for (int i = 0; i < str.length() / 2; i++) {
-            int n = str.length();
-            if (str.charAt(i) != str.charAt(n - 1 - i)) {
-                return false;
+//     public static boolean isPalindrome(String str) {
+//         for (int i = 0; i < str.length() / 2; i++) {
+//             int n = str.length();
+//             if (str.charAt(i) != str.charAt(n - 1 - i)) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+
+//     public static void main(String[] args) {
+//         String str = "racecar";
+//         System.out.println(isPalindrome(str));
+//     }
+// }
+
+import java.util.*;
+
+public class Strings {
+    public static float getShortestPath(String path){
+        int x = 0, y=0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+
+            //south
+            if(dir == 'S'){
+                y--;
+            }
+            //north
+            else if(dir == 'N'){
+                y++;
+            }
+            //west
+            else if(dir == 'W'){
+                x--;
+            }
+            //east
+            else{
+                x++;
             }
         }
 
-        return true;
+
+        int X2 = x*x;
+        int Y2 = y*y;
+
+        return (float)Math.sqrt(X2+Y2);
+
     }
 
     public static void main(String[] args) {
-        String str = "racecar";
-        System.out.println(isPalindrome(str));
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
     }
 }
