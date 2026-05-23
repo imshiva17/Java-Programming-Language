7635b2c988a3afd834a99cdf75903b0c174908e0
imshiva17 <shivarajak277@gmail.com>
2026-05-23 07:35:59 +0530

    String convert each the first letter of each word to uppercase

--- Strings.java
+++ Strings.java
@@ -219,17 +219,49 @@
 
 // Ex:
 
+// import java.util.*;
+
+// public class Strings {
+
+//     public static void main(String[] args) {
+//         StringBuilder sb = new StringBuilder("");
+
+//         for (char i = 'a'; i <= 'z'; i++) {
+//             sb.append(i);
+//         }
+
+//         System.out.println(sb);
+//     }
+// }
+
+//For a given String convert each the first letter of each word to uppercase
+
 import java.util.*;
 
 public class Strings {
-
-    public static void main(String[] args) {
+    public static String toUpperCase(String str) {
         StringBuilder sb = new StringBuilder("");
 
-        for (char i = 'a'; i <= 'z'; i++) {
-            sb.append(i);
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (char i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+
+                sb.append(Character.toUpperCase(str.charAt(i)));
+
+            } else {
+                sb.append(str.charAt(i));
+            }
         }
 
-        System.out.println(sb);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "hi, i am shiva";
+        System.out.println(toUpperCase(str));
     }
 }
