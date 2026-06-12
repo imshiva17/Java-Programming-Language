928248f1d3c06590dbdfe19c40b5b215a010610d
imshiva17 <shivarajak277@gmail.com>
2026-06-12 22:23:00 +0530

    Sorting

--- Sorting.java
+++ Sorting.java
@@ -509,32 +509,32 @@
 // Output: 1
 // Explanation: The smallest positive integer 1 is missing.
 
-import java.util.*;
+// import java.util.*;
 
-public class Sorting {
-    public static int firstMissingPositive(int[] nums) {
-        int i=0;
-        while(i<nums.length){
-            int correct = nums[i]-1;
-            if(nums[i]> 0 && nums[i] <= nums.length && nums[i] != nums[correct]){
-                int temp = nums[i];
-                nums[i] = nums[correct];
-                nums[correct] = temp;
-            }else{
-                i++;
-            }
-        }
+// public class Sorting {
+//     public static int firstMissingPositive(int[] nums) {
+//         int i=0;
+//         while(i<nums.length){
+//             int correct = nums[i]-1;
+//             if(nums[i]> 0 && nums[i] <= nums.length && nums[i] != nums[correct]){
+//                 int temp = nums[i];
+//                 nums[i] = nums[correct];
+//                 nums[correct] = temp;
+//             }else{
+//                 i++;
+//             }
+//         }
 
-        for (int j = 0; j < nums.length; j++) {
-            if(nums[j] != j+1){
-                return j+1;
-            }
-        }
-        return nums.length+1;
-    }
+//         for (int j = 0; j < nums.length; j++) {
+//             if(nums[j] != j+1){
+//                 return j+1;
+//             }
+//         }
+//         return nums.length+1;
+//     }
 
-    public static void main(String[] args) {
-        int nums[] = {3,4,-1,1};
-        System.out.println(firstMissingPositive(nums));
-    }
-} 
+//     public static void main(String[] args) {
+//         int nums[] = {3,4,-1,1};
+//         System.out.println(firstMissingPositive(nums));
+//     }
+// } 
